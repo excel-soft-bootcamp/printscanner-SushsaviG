b892084c726adb346a6d86120597bac22d4a893e
Venugopal K <venu.shastri@gmail.com>
2021-07-02 18:19:39 +0530

    Update PrintScannerProblem.cs

--- PrintScannerProblem.cs
+++ PrintScannerProblem.cs
@@ -1,30 +1,49 @@
-public class Printer
+public interface IPrinter{
+    void Print();
+}
+
+public interface IScanner{
+  void Scan();   
+}
+
+public class NanoLaserPrinter:IPrinter{
+ 
+    public void Print(){Console.WriteLine("Nano Laser Printer in Action");
+    
+}
+public class Printer:IPrinter
     {
         public void Print() { Console.WriteLine("Print in Action"); }
     }
 
-    public class Scanner
+    public class Scanner:IScanner
     {
         public void Scan() { Console.WriteLine("Scan In Action"); }
 
     }
 
     
-    public class PrintScanner
+    public class PrintScanner:IPrinter,IScanner
     {
-
+            public void Print(){
+            //Delegate Call To Either NanoLaserPrinter or Printer
+            }
+            public void Scan(){
+            //Delegate Call To Scanner
+            }
+            
     }
 
     public class PrintManager
     {
-        public void PrintDocument(Printer printer)
+        public void PrintDocument(IPrinter printer)
         {
             printer.Print();
         }
     }
     public class ScanManager
     {
-        public void ScanDocument(Scanner scaner)
+        public void ScanDocument(IScanner scaner)
         {
             scaner.Scan();
         }
@@ -46,6 +65,7 @@ public class Printer
 
             PrintScanner _printScanner = new PrintScanner();
             _printManager.PrintDocument(_printScanner);
+            
             _scanManager.ScanDocument(_printScanner);
 
 
